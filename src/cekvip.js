fd288dcb0a8585743c0f9788224d093ebb0f7478
Break01 <81243640+Break01@users.noreply.github.com>
2021-04-14 11:59:29 -0300

    Update cekvip.js

--- src/cekvip.js
+++ src/cekvip.js
@@ -1,7 +1,7 @@
 const cekvip = () => { 
 	return `           
 ──────────────────
-*Nome do bot* :  CAUSS BOT
+*Nome do bot* :  LEIAK BOT
 ──────────────────
         『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
 ──────────────────
