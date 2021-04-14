ef30eea649a1a97853af955431c3021055b4d831
Break01 <81243640+Break01@users.noreply.github.com>
2021-04-14 10:46:10 -0300

    Update menu2.js

--- src/menu2.js
+++ src/menu2.js
@@ -45,6 +45,6 @@ const menu2 = (prefix) => {
 ╠➸ *${prefix}simih 0 (para desativar)*
 ╠➥ *${prefix}simi (sua mensagem)*
 ║
-╠══✪〘 *CAUSS BOT* 〙✪══`
+╠══✪〘 * LEIAKBOT * 〙✪══`
 }
 exports.menu2 = menu2
